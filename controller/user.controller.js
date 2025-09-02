232dd27ba26807e7cce6a369754f1d5e68177013
bhanumetal <101553709+bhanumetal@users.noreply.github.com>
2025-09-02 18:18:02 +0530

    add bcryptjs
    - add bcryptjs

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,5 @@
 const UserModel = require("../model/user.model");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs");
 const path = require("path")
 const jwt = require("jsonwebtoken")
 
