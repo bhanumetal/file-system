374d59ad7894f29398d55297112a9f7322ca9970
bhanumetal <101553709+bhanumetal@users.noreply.github.com>
2025-09-03 09:00:45 +0530

    add vercel
    add vercel

--- api/index.js
+++ api/index.js
@@ -11,28 +11,28 @@ const express = require("express");
 // const storage = multer.memoryStorage();
 // const upload = multer({ storage });
 
-const {
-  signup,
-  login,
+// const {
+  // signup,
+  // login,
   // updateImage,
   // fetchImage,
-} = require("./controller/user.controller");
-const {
+// } = require("./controller/user.controller");
+// const {
   // createFile,
   // fetchFiles,
   // deleteFile,
   // downloadFile,
-} = require("./controller/file.controller");
-const { fetchDashboard } = require("./controller/dashboard.controller");
+// } = require("./controller/file.controller");
+// const { fetchDashboard } = require("./controller/dashboard.controller");
 // const { verifyToken } = require("./controller/token.controller");
-const {
-  shareFile,
+// const {
+  // shareFile,
   // fetchShared,
-  sendMail,
-} = require("./controller/share.controller");
-const AuthMiddleware = require("./middleware/auth.middleware");
+  // sendMail,
+// } = require("./controller/share.controller");
+// const AuthMiddleware = require("./middleware/auth.middleware");
 const app = express();
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -43,8 +43,8 @@ app.get("/api", (req, res) => {
   res.status(200).json({ message: "connet" });
 });
 
-app.post("/api/signup", signup);
-app.post("/api/login", login);
+// app.post("/api/signup", signup);
+// app.post("/api/login", login);
 // app.post(
 //   "/api/profile-picture",
 //   AuthMiddleware,
@@ -56,11 +56,11 @@ app.post("/api/login", login);
 // app.get("/api/file", AuthMiddleware, fetchFiles);
 // app.delete("/api/file/:id", AuthMiddleware, deleteFile);
 // app.get("/api/file/download/:id", downloadFile);
-app.get("/api/dashboard", AuthMiddleware, fetchDashboard);
+// app.get("/api/dashboard", AuthMiddleware, fetchDashboard);
 
-app.post("/api/send-mail", AuthMiddleware, sendMail);
+// app.post("/api/send-mail", AuthMiddleware, sendMail);
 // app.post("/api/token/verify", verifyToken);
-app.post("/api/share", AuthMiddleware, shareFile);
+// app.post("/api/share", AuthMiddleware, shareFile);
 // app.get("/api/share", AuthMiddleware, fetchShared);
 
 // Not found
