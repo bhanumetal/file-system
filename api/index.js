8a7fe9cc9f4b1b0706f693a0b0a0c19406d172af
bhanumetal <101553709+bhanumetal@users.noreply.github.com>
2025-09-03 09:22:39 +0530

    login
    - add login

--- api/index.js
+++ api/index.js
@@ -7,31 +7,31 @@ mongoose.connect(process.env.DB);
 // const root = process.cwd();
 const express = require("express");
 const serverless = require("serverless-http");
-// const multer = require("multer");
+const multer = require("multer");
 
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage });
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
-// const {
-  // signup,
-  // login,
-  // updateImage,
-  // fetchImage,
-// } = require("./controller/user.controller");
-// const {
-  // createFile,
-  // fetchFiles,
-  // deleteFile,
-  // downloadFile,
-// } = require("./controller/file.controller");
-// const { fetchDashboard } = require("./controller/dashboard.controller");
+const {
+  signup,
+  login,
+   updateImage,
+  fetchImage,
+} = require("./controller/user.controller");
+const {
+  createFile,
+  fetchFiles,
+  deleteFile,
+  downloadFile,
+} = require("./controller/file.controller");
+const { fetchDashboard } = require("./controller/dashboard.controller");
 // const { verifyToken } = require("./controller/token.controller");
 // const {
   // shareFile,
   // fetchShared,
   // sendMail,
 // } = require("./controller/share.controller");
-// const AuthMiddleware = require("./middleware/auth.middleware");
+const AuthMiddleware = require("./middleware/auth.middleware");
 const app = express();
 // app.listen(process.env.PORT || 8080);
 
@@ -44,20 +44,20 @@ app.get("/api", (req, res) => {
   res.status(200).json({ message: "connet" });
 });
 
-// app.post("/api/signup", signup);
-// app.post("/api/login", login);
-// app.post(
-//   "/api/profile-picture",
-//   AuthMiddleware,
-//   upload.single("picture"),
-//   updateImage
-// );
-// app.get("/api/profile-picture", AuthMiddleware, fetchImage);
-// app.post("/api/file", AuthMiddleware, upload.single("file"), createFile);
-// app.get("/api/file", AuthMiddleware, fetchFiles);
-// app.delete("/api/file/:id", AuthMiddleware, deleteFile);
-// app.get("/api/file/download/:id", downloadFile);
-// app.get("/api/dashboard", AuthMiddleware, fetchDashboard);
+app.post("/api/signup", signup);
+app.post("/api/login", login);
+app.post(
+  "/api/profile-picture",
+  AuthMiddleware,
+  upload.single("picture"),
+  updateImage
+);
+app.get("/api/profile-picture", AuthMiddleware, fetchImage);
+app.post("/api/file", AuthMiddleware, upload.single("file"), createFile);
+app.get("/api/file", AuthMiddleware, fetchFiles);
+app.delete("/api/file/:id", AuthMiddleware, deleteFile);
+app.get("/api/file/download/:id", downloadFile);
+app.get("/api/dashboard", AuthMiddleware, fetchDashboard);
 
 // app.post("/api/send-mail", AuthMiddleware, sendMail);
 // app.post("/api/token/verify", verifyToken);
