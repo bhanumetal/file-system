92d44314a3ad3945ae63e09fb502befdd79b1c1d
bhanumetal <101553709+bhanumetal@users.noreply.github.com>
2025-09-04 11:28:13 +0530

    set profile picture

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,91 +1,84 @@
 const UserModel = require("../model/user.model");
 const bcrypt = require("bcryptjs");
-const path = require("path")
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const { uploadFromBuffer } = require("../middleware/uploadFile.middleware");
 
-const signup = async (req, res)=>{
-    try {
-        await UserModel.create(req.body)
-        res.status(200).json({message: 'Signup success'})
-    }
-    catch(err)
-    {
-        res.status(500).json({message: err.message})
-    }
-}
+const signup = async (req, res) => {
+  try {
+    await UserModel.create(req.body);
+    res.status(200).json({ message: "Signup success" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-const login = async (req, res)=>{
-    try {
-        const {email, password} = req.body
-        const user = await UserModel.findOne({email: email})
-        
-        if(!user)
-            return res.status(404).json({message: "User doesn`t exist"})
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email: email });
 
-        const isLogin = bcrypt.compareSync(password, user.password)
-        
-        if(!isLogin)
-            return res.status(401).json({message: 'Incorrect password'})
+    if (!user) return res.status(404).json({ message: "User doesn`t exist" });
 
-        const payload = {
-            email: user.email,
-            mobile: user.mobile,
-            fullname: user.fullname,
-            id: user._id
-        }
+    const isLogin = bcrypt.compareSync(password, user.password);
 
-        const token = await jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '7d'})
+    if (!isLogin)
+      return res.status(401).json({ message: "Incorrect password" });
 
-        res.status(200).json({
-            message: 'Login success',
-            token: token
-        })
-    }
-    catch(err)
-    {
-        res.status(500).json({message: err.message})
-    }
-}
+    const payload = {
+      email: user.email,
+      mobile: user.mobile,
+      fullname: user.fullname,
+      id: user._id,
+    };
 
-const updateImage = async (req, res)=>{
-    try {
-        const {filename} = req.file
-        const user = await UserModel.findByIdAndUpdate(req.user.id, {image: filename})
+    const token = await jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
-        if(!user)
-            return res.status(401).json({message: 'Invalid request'})
+    res.status(200).json({
+      message: "Login success",
+      token: token,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
-        res.status(200).json({image: user.image})
-    }
-    catch(err)
-    {
-        res.status(500).json({message: err.message})
-    } 
-}
+const updateImage = async (req, res) => {
+  try {
+    const filename = req.file;
+    const result = await uploadFromBuffer(
+      filename.buffer,
+      filename.originalname
+    );
 
-const fetchImage = async (req, res)=>{
-    try {
-        const {image} = await UserModel.findById(req.user.id)
-        
-        if(!image)
-            return res.status(404).json({message: 'Image not found'})
+    const user = await UserModel.findByIdAndUpdate(req.user.id, {
+      image: result.secure_url,
+    });
 
-        const root = process.cwd()
-        const file = path.join(root, "files", image)
-        res.sendFile(file, (err)=>{
-            if(err)
-                res.status(404).json({message: 'Image not found'})
-        })
-    }
-    catch(err)
-    {
-        res.status(500).json({message: err.message})
-    } 
-}
+    if (!user) return res.status(401).json({ message: "Invalid request" });
+
+    res.status(200).json({ image: user.image });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const fetchImage = async (req, res) => {
+  try {
+    const { image } = await UserModel.findById(req.user.id);
+
+    if (!image) return res.status(404).json({ message: "Image not found" });
+
+    return res.redirect(image);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 module.exports = {
-    signup,
-    login,
-    updateImage,
-    fetchImage
-}
+  signup,
+  login,
+  updateImage,
+  fetchImage,
+};
